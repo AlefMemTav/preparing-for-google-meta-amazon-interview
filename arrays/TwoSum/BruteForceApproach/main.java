632913f3defb7fb139d776e635f97911e1981514
Pedro Lucas Garcia אמת <190115548@aluno.unb.br>
2025-06-28 20:55:34 -0300

    main.java

---
+++ arrays/TwoSum/BruteForceApproach/main.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int l = nums.length;
+        for(int i = 0; i < l; i++) {
+            int a = nums[i];
+            for(int j = i + 1; j < l; j++) {
+                int b = nums[j];
+                if(a + b == target){
+                    return new int[] {i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No solution");
+    }
+}
