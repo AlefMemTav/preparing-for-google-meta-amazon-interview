988253638444fb6030c5a42204921bac7749f156
Pedro Lucas Garcia אמת <190115548@aluno.unb.br>
2025-06-30 18:17:52 -0300

    Remove variável de two sum e atualiza nome de arquivo

--- arrays/TwoSum/BruteForce/bruteForce.java
+++ arrays/TwoSum/BruteForce/bruteForce.java
@@ -2,10 +2,8 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         int l = nums.length;
         for(int i = 0; i < l; i++) {
-            int a = nums[i];
             for(int j = i + 1; j < l; j++) {
-                int b = nums[j];
-                if(a + b == target){
+                if(nums[i] + nums[j] == target){
                     return new int[] {i, j};
                 }
             }
